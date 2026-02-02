2c175f670f61f7d507b9115275333453e2b319d1
Alberto-Codes <alberto.codes.dev@gmail.com>
2026-02-01 23:52:38 -0700

    feat(backends): add initial documentation for encryption backend implementations

---
+++ src/adk_secure_sessions/backends/__init__.py
@@ -0,0 +1,11 @@
+"""Encryption backend implementations for adk-secure-sessions.
+
+Examples:
+    Import the Fernet backend::
+
+        from adk_secure_sessions.backends.fernet import FernetBackend
+
+See Also:
+    [`adk_secure_sessions.backends.fernet`][adk_secure_sessions.backends.fernet]:
+    Fernet symmetric encryption backend.
+"""
